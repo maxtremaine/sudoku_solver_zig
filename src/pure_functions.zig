8fadf6441cd393a406ab5b6ae6ae0e61fc8af569
Max Tremaine <max.tremaine@gmail.com>
2023-08-25 15:11:04 -0400

    Updated for 170

--- src/pure_functions.zig
+++ src/pure_functions.zig
@@ -2,9 +2,17 @@ const expect = @import("std").testing.expect;
 
 pub fn hasDuplicates(list: [9]u8) bool {
     var pastValues: [9]u8 = undefined;
+    var check170 = false;
     for(list) |value, i| {
         if(i == 0) {
             pastValues[0] = value;
+            continue;
+        }
+        if(value == 170) {
+            if(check170) {
+                return true;
+            }
+            check170 = true;
             continue;
         }
         for(pastValues) |pastValue| {
@@ -18,7 +26,7 @@ pub fn hasDuplicates(list: [9]u8) bool {
 }
 
 test "identifies lists with duplicates" {
-    const noDupes = [_]u8{ 170, 2, 3, 0, 5, 6, 7, 8, 9 };
+    const noDupes = [_]u8{ 1, 170, 3, 0, 5, 6, 7, 8, 9 };
     const hasDupes = [_]u8{1, 2, 3, 3, 3, 3, 3, 3, 3 };
     try expect(hasDuplicates(noDupes) == false);
     try expect(hasDuplicates(hasDupes) == true);
